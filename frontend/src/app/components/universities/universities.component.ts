76299c4bb1dd67b3771eaa215db5e61d8775b5ff
Kam S <singhkam07@gmail.com>
2021-07-31 14:42:04 -0400

    Updated universities component to use the loading bar component and the value of debugMode in utils/config.ts

--- frontend/src/app/components/universities/universities.component.ts
+++ frontend/src/app/components/universities/universities.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { University } from 'src/app/models/university';
 import { RawDataService } from 'src/app/services/raw-data.service';
 import { contract } from 'src/app/utils/observable';
+import { debugMode } from 'src/app/utils/config';
 
 @Component({
   selector: 'app-universities',
@@ -11,15 +12,13 @@ import { contract } from 'src/app/utils/observable';
 export class UniversitiesComponent implements OnInit {
   universities: University[] = [];
   offset = 0;
-  limit = 15;
+  limit = 16;
   isLoading = false;
   completedResultSet = false;
-  debug = true;
 
   constructor(private _Database: RawDataService) { }
 
   ngOnInit(): void {
-    // this.loadData();
   }
 
   onScroll() {
@@ -33,13 +32,16 @@ export class UniversitiesComponent implements OnInit {
     try {
       let data = await contract(this._Database.getUniversities(this.limit, this.offset));
       let newUniversities = data.data;
-      // this.offset += this.limit;
-      // if(newUniversities.length < this.limit) {
-      //   this.completedResultSet = true;
-      // }
-      while (newUniversities.length < this.limit) {
-        newUniversities.push(...newUniversities);
-      }
+      if (!debugMode) {
+        this.offset += this.limit;
+        if(newUniversities.length < this.limit) {
+          this.completedResultSet = true;
+        }
+      } else {
+        while (newUniversities.length < this.limit) {
+          newUniversities.push(...newUniversities);
+        }
+      }      
       this.universities.push(...newUniversities);
       this.isLoading = false;
     } catch (err) {
