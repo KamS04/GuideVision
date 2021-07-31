a1ca923b662421445c03d3fcc36f538808ae6898
Kam S <singhkam07@gmail.com>
2021-07-31 12:21:37 -0400

    Setup generic universities component - not perfect but 90% there

--- frontend/src/app/components/universities/universities.component.ts
+++ frontend/src/app/components/universities/universities.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { University } from 'src/app/models/university';
+import { RawDataService } from 'src/app/services/raw-data.service';
+import { contract } from 'src/app/utils/observable';
 
 @Component({
   selector: 'app-universities',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./universities.component.css']
 })
 export class UniversitiesComponent implements OnInit {
+  universities: University[] = [];
+  offset = 0;
+  limit = 15;
+  isLoading = false;
+  completedResultSet = false;
+  debug = true;
 
-  constructor() { }
+  constructor(private _Database: RawDataService) { }
 
   ngOnInit(): void {
+    // this.loadData();
+  }
+
+  onScroll() {
+    if (!this.completedResultSet) {
+      this.loadData();
+    }
+  }
+
+  async loadData() {
+    this.isLoading = true;
+    try {
+      let data = await contract(this._Database.getUniversities(this.limit, this.offset));
+      let newUniversities = data.data;
+      // this.offset += this.limit;
+      // if(newUniversities.length < this.limit) {
+      //   this.completedResultSet = true;
+      // }
+      while (newUniversities.length < this.limit) {
+        newUniversities.push(...newUniversities);
+      }
+      this.universities.push(...newUniversities);
+      this.isLoading = false;
+    } catch (err) {
+      // TODO Error Handling
+      console.error(err);
+    }
   }
 
 }
