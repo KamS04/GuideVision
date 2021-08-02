506042abfb66c29def8751512831002de12b7673
Kam S <singhkam07@gmail.com>
2021-08-02 16:10:27 -0400

    Finished pathway single component to display details on single pathway

--- frontend/src/app/components/pathway-single/pathway-single.component.ts
+++ frontend/src/app/components/pathway-single/pathway-single.component.ts
@@ -1,15 +1,69 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Category } from 'src/app/models/category';
+import { MiniCourse } from 'src/app/models/course';
+import { ResultError } from 'src/app/models/result';
+import { RawDataService } from 'src/app/services/raw-data.service';
+import { contract } from 'src/app/utils/observable';
+import { AbstractError } from '../views/display-four-o-four/error.interface';
 
 @Component({
   selector: 'app-pathway-single',
   templateUrl: './pathway-single.component.html',
   styleUrls: ['./pathway-single.component.css']
 })
-export class PathwaySingleComponent implements OnInit {
+export class PathwaySingleComponent extends AbstractError implements OnInit {
+  pathwayId: number;
+  selectedCategory: Category = null;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  attachedCourses: MiniCourse[] = [];
+  isLoadingCourses: boolean = false;
+  
+  constructor(
+    private route: ActivatedRoute,
+    private _Database: RawDataService
+  ) {
+    super();
   }
 
+  ngOnInit(): void {
+    let urlId = parseInt( this.route.snapshot.paramMap.get('id') );
+    if (isNaN(urlId)) {
+      this.showError();
+    } else {
+      this.hideError();
+      this.pathwayId = urlId;
+      this.getPathwayToDisplay();
+    }    
+  }
+
+  async getPathwayToDisplay() {
+    try {
+      let data = await contract( this._Database.getCategory(this.pathwayId) );
+      this.selectedCategory = data.data;
+      this.hideError();
+    } catch (err: any) {
+      let resultErr = err as ResultError;
+      if (resultErr !== undefined) {
+        this.showError(resultErr.statusCode);
+      } else {
+        // TODO Error Handling
+      }
+    }
+    if (this.selectedCategory != null) {
+      this.loadCourses();
+    }
+  }
+
+  async loadCourses() {
+    this.isLoadingCourses = true;
+    try {
+      let data = await contract( this._Database.getMinifiedCourseForCategory(this.pathwayId) );
+      this.attachedCourses = data.data;
+    } catch (err) {
+      // TODO Error handling
+      console.error(err);
+    }
+    this.isLoadingCourses = false;
+  }
 }
