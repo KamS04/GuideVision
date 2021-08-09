0b2a6d30fb652d0228497f85c283cbbc2353a16b
Kam S <singhkam07@gmail.com>
2021-08-09 16:26:16 -0400

    Created over-arching programs list in compare component, allowing users to remove programs from the compare list

--- frontend/src/app/components/compare/compare.component.ts
+++ frontend/src/app/components/compare/compare.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from 'src/app/models/course';
+import { CompareDataService } from 'src/app/services/compare-data.service';
 
 @Component({
   selector: 'app-compare',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./compare.component.css']
 })
 export class CompareComponent implements OnInit {
+  selectedCourses: Course[];
+  numberOfCourses: number = 0;
 
-  constructor() { }
+  constructor(
+    private _CompareList: CompareDataService,
+  ) {
+    this.getCompareList();
+  }
 
   ngOnInit(): void {
+    this._CompareList.compareListChanged.subscribe( (length) => {
+      console.log(length);
+      this.getCompareList();
+    });
+  }
+
+  getCompareList() {
+    this.selectedCourses = this._CompareList.getCompareList();
+    this.numberOfCourses = this._CompareList.listLength;
+  }
+
+  magic(course, event) {
+    this._CompareList.removeCourse(course);
+    event.target.blur();
   }
 
 }
