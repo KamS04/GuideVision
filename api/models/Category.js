7ee18d781afe51b2b4b6bb9ed9273276f2bbb80d
Kam S <singhkam07@gmail.com>
2021-08-20 14:17:57 -0400

    models were moved from the /models to data/models to highlight tight coupling between data and models

--- api/models/Category.js
+++
@@ -1,7 +0,0 @@
-module.exports = class Category {
-    constructor(id, title, futureJobOpporunities) {
-        this.id = id;
-        this.title = title;
-        this.futureJobOpportunities = futureJobOpporunities;
-    }
-}
