6f774b667038786290f76ff4cdd1d51cc81b8a73
Kam S <singhkam07@gmail.com>
2021-08-20 14:18:43 -0400

    Modified app.js to reference the new architecture/naming

--- api/app.js
+++ api/app.js
@@ -1,38 +1,25 @@
 const express = require('express');
 const app = express();
 
-const courseRoute = require('./routes/course');
-const universityRoute = require('./routes/university');
-const categoryRoute = require('./routes/category');
+const programsRouter = require('./routes/program');
+const universitiesRouter = require('./routes/university');
+const pathwaysRouter = require('./routes/pathway');
 
 const config = require('./config');
 
 app.use(express.static('./public'));
 
-app.get('/', (req, res) => {
-    res.json({ success: true, msg: 'Hello there.'});
-});
-
-// const apiRouter = express.Router();
-
 if (config.DEBUG) {
-    app.use((req, res, next) => {
-        console.log(req.url, req.query);
+    app.use( (req, res, next) => {
+        console.log(req.url, req.params, req.query);
         next();
     });
 }
 
-// apiRouter.use('/course', courseRoute);
-// apiRouter.use('/university', universityRoute);
-// apiRouter.use('/category', categoryRoute)
-
-
-// app.use('/api', apiRouter);
-
-app.use('/api/course', courseRoute);
-app.use('/api/university', universityRoute);
-app.use('/api/category', categoryRoute);
+app.use('/api/programs', programsRouter);
+app.use('/api/universities', universitiesRouter);
+app.use('/api/pathways', pathwaysRouter);
 
 app.listen(config.PORT, () => {
-    console.log(`Server listening on ${config.PORT}...`)
+    console.log(`Server listening on ${config.PORT}...`);
 });
