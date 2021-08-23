d105650634bd8b40d16887efc0c1d730e74595fa
Kam S <singhkam07@gmail.com>
2021-08-23 12:30:42 -0400

    Realized that environment variables are always strings so, started parsing them

--- api/config.js
+++ api/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    DEBUG: process.env.DEBUG || true,
-    PORT: process.env.PORT || 9200,
+    DEBUG: Boolean(process.env.DEBUG || true),
+    PORT: parseInt(process.env.PORT || 9200, 10),
     DB_FILE: process.env.DB_FILE || './guidevision.db'
 }
