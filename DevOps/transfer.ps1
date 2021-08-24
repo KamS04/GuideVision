f45d165419c81aff3a8981f9589e5884f43cb644
Kam S <singhkam07@gmail.com>
2021-08-24 14:38:12 -0400

    Checked database transfer scripts into git

---
+++ DevOps/transfer.ps1
@@ -0,0 +1,44 @@
+$server = 'homeserver' # The Server to ssh into
+
+# Where the temporary dump file will be created on the local machine
+$outputFile = 'D:\Code\web\Projects\CAS\GuideVision\output.sql'
+# The location of the local database from which the dump will be created
+$database = 'D:\Code\web\Projects\CAS\GuideVision\DB-Browser\database.db' 
+
+# Dump the database contents into a dump file
+sqlite3 $database .dump | Set-Content -Path $outputFile
+
+# Replace some stuff in the dump file
+$createOriginal = "CREATE TABLE"
+$createNew = "CREATE TABLE IF NOT EXISTS"
+
+$insertOriginal = "INSERT INTO"
+$insertNew = "INSERT OR REPLACE INTO"
+
+(Get-Content -Path $outputFile ) -replace $createOriginal, $createNew -replace $insertOriginal, $insertNew | Set-Content -Path $outputFile
+
+# Where the overall GuideVision directory is located
+$baseDirOnServer = "~/Documents/GuideVision"
+
+$baseDir = ssh $server "realpath $baseDirOnServer"
+
+$outputFileOnServer = "$baseDir/output.sql"
+
+# Send the dump file to the server
+scp $outputFile $server:$outputFileOnServer
+
+Remove-Item -Path $outputFile
+
+$databaseOnServer = "$baseDir/api/guidingvision.db"
+
+$tempCommandFile = "./temp_comms.sh"
+
+$appName="GuidingVision"
+
+# Replace the paths in the comms template
+Get-Content -Path "./commands.template.sh" -replace "PUT_DATABASE_PATH_HERE", $databaseOnServer -replace "PUT_OUTPUT_FILE_HERE", $outputFileOnServer -replace "PUT_APP_NAME_HERE", $appName | Set-Content -Path $tempCommandFile
+
+# run the commands on the server
+Get-Content -Path $tempCommandFile | ssh $server
+
+Remove-Item -Path $tempCommandFile
---
+++ DevOps/transfer.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# The Server to ssh into
+server='homeserver'
+
+# Where the temporary dump file will be created on the local machine
+outputFile='D:\\Code\\web\\Projects\\CAS\\GuideVision\\output.sql'
+# The location of the local database from which the dump will be created
+database='D:\\Code\\web\\Projects\\CAS\\GuideVision\\DB-Browser\\database.db'
+
+# Dump the database contents into a dump file
+sqlite3 $database .dump > $outputFile
+
+# Replace some stuff in the dump file
+createOriginal='CREATE TABLE'
+createNew='CREATE TABLE IF NOT EXISTS'
+
+insertOriginal='INSERT INTO'
+insertNew='INSERT OR REPLACE INTO'
+
+sed -i -e "s/$createOriginal/$createNew/" -e "s/$insertOriginal/$insertNew/" $outputFile
+
+# Where the overall GuideVision directory is located on the server
+baseDirOnServer='~/Documents/GuideVision'
+
+baseDir=$(ssh $server "realpath $baseDirOnServer")
+
+outputFileOnServer="$baseDir/output.sql"
+
+# Send the dump file to the server
+scp $outputFile $server:$outputFileOnServer
+
+rm $outputFile
+
+databaseOnServer="$baseDir/api/guidingvision.db"
+
+tempCommandFile='./temp_comms.sh'
+
+appName='GuidingVision'
+
+cp "./commands.template.sh" $tempCommandFile
+
+# Replace the paths in the comms template
+sed -i -e "s/PUT_DATABASE_PATH_HERE/$databaseOnServer/" -e "s/PUT_OUTPUT_FILE_HERE/$outputFileOnServer/" -e "s/PUT_APP_NAME_HERE/$appName/" $tempCommandFile
+
+# Run the commands on the server
+cat $tempCommandFile | ssh $server
+
+rm $tempCommandFile
