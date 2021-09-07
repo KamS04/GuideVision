12a7a0dbeae334f278ba99afd5d56d8c9c7274f0
Kam S <singhkam07@gmail.com>
2021-09-07 13:09:39 -0400

    Added full name to about footer

--- frontend/src/app/components/about-footer/about-footer.component.ts
+++ frontend/src/app/components/about-footer/about-footer.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutFooterComponent implements OnInit {
 
-  members = ['Kamalpreet', 'Ayaan', 'Arshnur', 'Parneet', 'Harsh'];
+  members = ['Kamalpreet Singh', 'Ayaan Nanji', 'Arshnur Ahluwalia', 'Parneet Singh', 'Harsh Nair'];
   
   linkToInstagram = 'https://www.instagram.com/guiding.vision/';
   email = 'guidingvision.help@gmail.com';
